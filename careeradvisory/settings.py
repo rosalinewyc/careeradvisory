0fe49fb3f07241170dccb014055130a1cf4e4f1f
Rosaline <yuchun.wang.2015@smu.edu.sg>
2017-06-17 22:16:14 +0800

    Edited

--- careeradvisory/settings.py
+++ careeradvisory/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'careeradvisory.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            '/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
