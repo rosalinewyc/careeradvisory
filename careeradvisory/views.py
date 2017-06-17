8f4899fbc53d611f6696a3b18a877a9159491a85
Rosaline <yuchun.wang.2015@smu.edu.sg>
2017-06-17 22:05:18 +0800

    Edited

--- careeradvisory/views.py
+++ careeradvisory/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect
 
 def login(request): # direct to login. html upon clicking logout
     return HttpResponseRedirect('login')
