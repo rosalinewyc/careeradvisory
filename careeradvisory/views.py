e206bb4525979751ec0b4e6cb7a99e6d67edc5bb
Rosaline <yuchun.wang.2015@smu.edu.sg>
2017-06-17 21:59:04 +0800

    Edited

--- careeradvisory/views.py
+++ careeradvisory/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
-def login(): # direct to login. html upon clicking logout
+def login(request): # direct to login. html upon clicking logout
     return HttpResponseRedirect('login')
